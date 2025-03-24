56fdb5423c757d426db444f705417a95851b593f
DvGuerraa <davimguerra@gmail.com>
2025-03-24 12:31:11 -0300

    CriarArquivosDeFormaDinamica

--- ler_nomes.php
+++ ler_nomes.php
@@ -3,16 +3,14 @@
 //filesystem
 //f(algumacoisa) - file(arquivo)
 //read (ler) - r
+//write (escrever) - w
 //resource = recurso
 //eof = end of file (fim do arquivo)
 //fwrite - write (escrever)
-//truncar - truncate (truncar) -> "w"
-//append - append (acrescentar)
+//truncar - truncate
+//append - append (acrescentar) - criar o arquivo se ele não existir
 
 $arquivo = fopen("nomes.txt", "a");
-
-fwrite($arquivo, "\n");
-fwrite($arquivo, 'Cristiano');
 
 
 fclose($arquivo);
