e1c4210d616a3fa09ceb73b6d1f92be6a28e0150
DvGuerraa <davimguerra@gmail.com>
2025-03-24 12:03:01 -0300

    Feof

--- ler_nomes.php
+++ ler_nomes.php
@@ -4,6 +4,7 @@
 //f(algumacoisa) - file(arquivo)
 //read (ler) - r
 //resource = recurso
+//eof = end of file (fim do arquivo)
 
 $arquivo = fopen("nomes.txt", "r");
 
@@ -11,8 +12,10 @@ $bytes = filesize('nomes.txt');
 
 //ler o conteúdo do arquivo
 
-while(($linha = fgets($arquivo, $bytes)) !== false) {
-            echo "$linha</br>";
-        }
+while(!feof($arquivo)){
+    $nome = fgets($arquivo);
+    echo $nome;
+    echo '</br>';
+}
 
 fclose($arquivo);
