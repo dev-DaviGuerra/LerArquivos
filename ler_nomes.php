e38b4e9c9de733975468f90395462df60c580cb3
DvGuerraa <davimguerra@gmail.com>
2025-03-25 13:07:00 -0300

    functionŔealPath()

--- ler_nomes.php
+++ ler_nomes.php
@@ -1,10 +1,22 @@
 <?php
 
-//caminho absoluto: $arquivo = fopen("./pasta/nomes.txt", "r")
- 
-//caminho relativo: $arquivo = fopen("nomes.txt", "r");
+//filesystem
+ //f(algumacoisa) - file(arquivo)
+ //read (ler) - r
+ //write (escrever) - w
+ //resource = recurso
+ //eof = end of file (fim do arquivo)
+ //fwrite - write (escrever)
+ //truncar - truncate (truncar) -> "w"
+ //append - append (acrescentar)
+ //truncar - truncate
+ //append - append (acrescentar) - criar o arquivo se ele não existir
 
-$relativePath = 'pasta/nomes.txt';
+$relativePath = 'dados/nomes.txt';
 $absolutePath = realpath($relativePath);
 
-fclose($arquivo);
+if(file_exists($absolutePathabs)) {
+    echo 'Arquivo existe';
+}
+echo $absolutePath;
+ 
