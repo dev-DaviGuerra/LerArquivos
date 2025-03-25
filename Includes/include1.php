c778976a94e9701f3d091a0d482d0dac178ce8e3
DvGuerraa <davimguerra@gmail.com>
2025-03-25 13:17:13 -0300

    MagicVarPath

---
+++ Includes/include1.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'O retorno de include1 __DIR__ é o seguinte: '.__DIR__;
+echo '<br>';
+echo 'O retorno de include1 __FILE__ é o seguinte: '.__FILE__;
+echo 'O retorno de getcwd() em include1 é o seguinte: '. getcwd();
---
+++ path.php
@@ -0,0 +1,12 @@
+<?php
+
+include('includes/include1.php');
+
+
+
+echo '<hr>';
+echo 'O retorno de path __DIR__ é o seguinte: '.__DIR__;
+echo '<br>';
+echo 'O retorno de path __FILE__ é o seguinte: '.__FILE__;
+echo '<br>';
+echo 'O retorno de getcwd() em path é o seguinte: '. getcwd();
